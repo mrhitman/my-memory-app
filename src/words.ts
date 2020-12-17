f5446aad93a5140d063fffd447dd71d92225663a
One <kabalx47@gmail.com>
2020-12-17 10:51:19 +0200

    fix picture

--- src/words.ts
+++ src/words.ts
@@ -169,7 +169,7 @@ export const words: Word[] = [
     index: 29,
     text: "Тур",
     url:
-      "https://thumbs.dreamstime.com/b/primigenius-%D0%B1%D1%8B%D0%BA%D0%B0-%D1%82%D1%83%D1%80-48493043.jpg"
+      "https://zooclub.ru/attach/wild/690.jpg"
   },
   {
     index: 30,
